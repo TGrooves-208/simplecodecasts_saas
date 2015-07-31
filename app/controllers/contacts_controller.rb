d265801daacf747899b019c14a9852b41f45c25b
TGrooves-208 <tgrooves.208@gmail.com>
2015-07-30 21:50:41 -0600

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,17 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(contact_params)
 
 		if @contact.save
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			body = params[:contact][:comments]
+
+			ContactMailer.contact_email(name, email, body).deliver
+			
 			flash[:success] = "Message sent."
-			redirect_to new_contact_path, notice: "Message sent."
+			redirect_to new_contact_path
 		else
-			flash[:danger] = "Error ccoured, message has not been sent."
-			redirect_to new_contact_path, notice: "Error occured."
+			flash[:error] = "Error ccoured, message has not been sent."
+			redirect_to new_contact_path
 	end
 end
 
